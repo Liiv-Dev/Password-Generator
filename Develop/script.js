d99f21d30afc045dd8ae06a6986ed95faa5efa3a
Willie Jones <williejones432@yahoo.com>
2023-09-21 12:06:17 -0500

    Added Acceptance Criteria to README

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var hasUpper = true;
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +11,29 @@ function writePassword() {
   passwordText.value = password;
 
   
+}
+
+function validateCriteria () {
+  
+}
+
+function validateLength(userLength) {
+  return false;
+}
+
+function generatePassword () {
+  var passlength = prompt("Enter password length 8-128 characters")
+  
+  console.log(passlength);
+
+  if (validateLength(passlength)) {
+
+  } else {
+    alert("You have entered and invalid password length")
+    return "";
+  }
+  
+  return newPass;
 }
 
 // Add event listener to generate button
