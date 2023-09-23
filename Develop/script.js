9f46453a5242ce7b6c180b2074d9f96f39a4335c
Willie Jones <williejones432@yahoo.com>
2023-09-22 22:04:45 -0500

    Updated Javascript

--- Develop/script.js
+++ Develop/script.js
@@ -9,32 +9,28 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
   
 }
 
-function validateCriteria () {
-  
-}
-
-function validateLength(userLength) {
-  return false;
-}
-
+//Function that generates new password
 function generatePassword () {
-  var passlength = prompt("Enter password length 8-128 characters")
-  
-  console.log(passlength);
+  var newPass = '';
+  var passlength = newPass.length;
+  //array of vaid characters to generate
+  var validChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","!","$","&","-","_","#","@","?", "1","2","3","4","5","6","7","8","9"];
 
-  if (validateLength(passlength)) {
-
-  } else {
-    alert("You have entered and invalid password length")
-    return "";
+  //Sets range of characters 
+  if (passlength < 8) {
+    prompt("Password must be 8-128 characters and include valid character types")
+  } else if (passlength > 128) {
+    prompt("Password must be 8-128 characters and include valid character types")
   }
+
   
   return newPass;
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
