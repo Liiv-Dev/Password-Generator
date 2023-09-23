5bb2c1be2c01d5f55bb7616df86c53c7d43925f5
Willie Jones <williejones432@yahoo.com>
2023-09-23 04:33:21 -0500

    Updated statements

--- Develop/script.js
+++ Develop/script.js
@@ -14,18 +14,23 @@ function writePassword() {
 
 //Function that generates new password
 function generatePassword () {
-  var newPass = '';
-  var passlength = newPass.length;
+  var newPass = '12345678910';
+  var passlength = 10;
   //array of vaid characters to generate
-  var validChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","!","$","&","-","_","#","@","?", "1","2","3","4","5","6","7","8","9"];
+  ///var validChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","!","$","&","-","_","#","@","?", "1","2","3","4","5","6","7","8","9"];
+
+  var validChar = ['abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]{}()|!@#$&']
 
   //Sets range of characters 
-  if (passlength < 8) {
+  if (newPass.length < 8) {
     prompt("Password must be 8-128 characters and include valid character types")
-  } else if (passlength > 128) {
+  } else if (newPass.length > 128) {
     prompt("Password must be 8-128 characters and include valid character types")
   }
 
+  /*for (let i = 0; i < passLength; i++)
+        newPass += validChar[i][Math.floor(Math.random() * validChar[i].length)];
+  */
   
   return newPass;
 }
